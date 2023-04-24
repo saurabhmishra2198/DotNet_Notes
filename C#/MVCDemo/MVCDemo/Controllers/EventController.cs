84fd210c361578011023c20be4af4cc060beeba3
saurabh mishra <mishrasaurabh350@gmail.com>
2023-04-24 23:45:36 +0530

    MVC project Add

---
+++ C#/MVCDemo/MVCDemo/Controllers/EventController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: EventController
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EventView()
+        {
+            return View();
+        }
+
+        // GET: EventController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EventController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EventController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EventController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ C#/MVCDemo/MVCDemo/Models/EventModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCDemo.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string? EventName { get; set; }
+        public string? EventType { get; set; }
+        public string? EventDescription { get; set; }
+        public decimal EventPrice { get; set; }
+    }
+}
