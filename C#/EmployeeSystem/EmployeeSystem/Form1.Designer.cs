7ca53c130ae51a26c97ddf7b28e5b77c39a1f68b
saurabh mishra <mishrasaurabh350@gmail.com>
2023-04-26 23:54:28 +0530

    New Application Add ADO.Net

---
+++ C#/EmployeeSystem/EmployeeSystem/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace EmployeeSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EmployeeId = new System.Windows.Forms.TextBox();
+            this.EmployeeName = new System.Windows.Forms.TextBox();
+            this.EmployeeJoiningDate = new System.Windows.Forms.TextBox();
+            this.EmployeeSalary = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Management System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee Id :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Employee Name :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Joining Date :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Salary :";
+            // 
+            // EmployeeId
+            // 
+            this.EmployeeId.Location = new System.Drawing.Point(395, 62);
+            this.EmployeeId.Name = "EmployeeId";
+            this.EmployeeId.Size = new System.Drawing.Size(100, 20);
+            this.EmployeeId.TabIndex = 5;
+            // 
+            // EmployeeName
+            // 
+            this.EmployeeName.Location = new System.Drawing.Point(395, 94);
+            this.EmployeeName.Name = "EmployeeName";
+            this.EmployeeName.Size = new System.Drawing.Size(100, 20);
+            this.EmployeeName.TabIndex = 6;
+            // 
+            // EmployeeJoiningDate
+            // 
+            this.EmployeeJoiningDate.Location = new System.Drawing.Point(395, 128);
+            this.EmployeeJoiningDate.Name = "EmployeeJoiningDate";
+            this.EmployeeJoiningDate.Size = new System.Drawing.Size(100, 20);
+            this.EmployeeJoiningDate.TabIndex = 7;
+            // 
+            // EmployeeSalary
+            // 
+            this.EmployeeSalary.Location = new System.Drawing.Point(395, 159);
+            this.EmployeeSalary.Name = "EmployeeSalary";
+            this.EmployeeSalary.Size = new System.Drawing.Size(100, 20);
+            this.EmployeeSalary.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(203, 269);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 150);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(203, 214);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 10;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(304, 214);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(75, 23);
+            this.Display.TabIndex = 11;
+            this.Display.Text = "Display";
+            this.Display.UseVisualStyleBackColor = true;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(395, 214);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 12;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(487, 214);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 13;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.EmployeeSalary);
+            this.Controls.Add(this.EmployeeJoiningDate);
+            this.Controls.Add(this.EmployeeName);
+            this.Controls.Add(this.EmployeeId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox EmployeeId;
+        private System.Windows.Forms.TextBox EmployeeName;
+        private System.Windows.Forms.TextBox EmployeeJoiningDate;
+        private System.Windows.Forms.TextBox EmployeeSalary;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Display;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
